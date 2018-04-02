be7f3e42b234760aa4011e4ccd31f996a21caa83
Wight554 <wight554@gmail.com>
2018-04-02 07:05:51 +0000

    msm8998-common: Import proper sepolicy
    * Kanged from https://github.com/Demon000/device_xiaomi_msm8998-common/tree/lineage-15.1/sepolicy

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -53,7 +53,7 @@ TARGET_NO_BOOTLOADER := true
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0xc1b0000 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=2048 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3
-#BOARD_KERNEL_CMDLINE +=androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE +=androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
@@ -256,12 +256,10 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(PLATFORM_PATH)
 # RIL
 PROTOBUF_SUPPORTED := true
 
-# selinux
+# SELinux
 include device/qcom/sepolicy/sepolicy.mk
-include vendor/omni/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/vendor
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/public
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/private
+
+BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
 
 BOARD_SECCOMP_POLICY += $(PLATFORM_PATH)/seccomp_policy
 
