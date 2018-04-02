fb07c2ef8e7e7f15a653b1a174c677d9710fb853
Wight554 <wight554@gmail.com>
2018-04-02 06:34:56 +0000

    configs: Align NFC configs with Lineage OS
    * Kanged from https://github.com/Demon000/device_xiaomi_msm8998-common/commit/7bdb1cf8cf669da1b23a8ab3e3d9b672c41dc211
    * Decommonize NXP configs

--- msm8998.mk
+++ msm8998.mk
@@ -261,8 +261,7 @@ PRODUCT_PACKAGES += \
     Tag
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
-    $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf
+    $(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf
 
 # OMX
 PRODUCT_PACKAGES += \
