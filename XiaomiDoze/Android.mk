1f0ea8a9d39addc441bdb6c7478b2147adcf3129
Wight554 <wight554@gmail.com>
2018-04-21 08:05:49 +0000

    msm8998-common: Update XiaomiDoze package
    * Break LineageOS dependency

--- XiaomiDoze/Android.mk
+++ XiaomiDoze/Android.mk
@@ -1,4 +1,5 @@
 LOCAL_PATH:= $(call my-dir)
+ifeq ($(TARGET_DEVICE),$(filter $(TARGET_DEVICE),chiron sagit))
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
@@ -17,17 +18,16 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-v7-recyclerview \
     android-support-v7-preference \
     android-support-v7-appcompat \
-    android-support-v14-preference \
-
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    org.lineageos.platform.internal
+    android-support-v14-preference
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
-    $(TOP)/packages/resources/devicesettings/res
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+LOCAL_DEX_PREOPT := false
 
 include $(BUILD_PACKAGE)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
