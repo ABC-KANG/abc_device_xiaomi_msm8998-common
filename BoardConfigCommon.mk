dcb9b2462afe190a88b95016dbc7beb15b7c02d5
Wight554 <wight554@gmail.com>
2018-05-16 14:55:20 +0000

    msm8998-common: switch to enforcing selinux
    * fix up sepolicy denials leftovers

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -44,7 +44,6 @@ BOARD_KERNEL_CMDLINE += user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=2048
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
