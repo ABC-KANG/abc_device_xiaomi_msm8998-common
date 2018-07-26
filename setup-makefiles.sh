9eeddeb4cab5b3618b94d377bd3e08907aadf2f3
Vol Zhdanov <wight554@gmail.com>
2018-07-26 12:19:37 +0000

    msm8998-common: aosip-ify

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -24,9 +24,9 @@ INITIAL_COPYRIGHT_YEAR=2017
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-ABC_ROOT="$MY_DIR"/../../..
+AOSIP_ROOT="$MY_DIR"/../../..
 
-HELPER="$ABC_ROOT"/scripts/tools/extract_utils.sh
+HELPER="$AOSIP_ROOT"/vendor/aosip/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -34,7 +34,7 @@ fi
 . "$HELPER"
 
 # Initialize the helper for common
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$ABC_ROOT" true
+setup_vendor "$DEVICE_COMMON" "$VENDOR" "$AOSIP_ROOT" true
 
 # Copyright headers and guards
 write_headers "chiron sagit"
@@ -47,7 +47,7 @@ write_footers
 
 if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
     # Reinitialize the helper for device
-    setup_vendor "$DEVICE" "$VENDOR" "$ABC_ROOT" false
+    setup_vendor "$DEVICE" "$VENDOR" "$AOSIP_ROOT" false
 
     # Copyright headers and guards
     write_headers
